f96241e982c5e5fe61e77098647c41e4cdc49a1d
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-09 20:50:02 -0600

    changed some functions

--- server.js
+++ server.js
@@ -1,48 +1,52 @@
-//======================================================================
-// Welcome to my Note-Taker, based on Express.js.
-
-// Below are my dependencies;
-//======================================================================
+// require dependencies;
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 let notes = require("./db/db.json")
 
-//======================================================================
-// This sets up the Express App
-//======================================================================
 
+//set the app
 const app = express();
+
+// create PORT
 const PORT = process.env.PORT || 3001;
 
-//==============================================================================
-// This sets up data parsing-- Express will interpret it/format data as JSON.
-// This is required for API calls!
-//==============================================================================
-
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-currentID = notes.length;
 
-//==============================================================================
-// On page load, it should start with index.html. First get it and then listen.
-//==============================================================================
-// Since the GET and POST functions grab from the same route, we can set it once up here.
 
+// Get the notes from db json
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/", function (req, res) {
+res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
+
+// Get the notes from db json
 app.get("/api/notes", function (req, res) {
 
     return res.json(notes);
 });
 
-app.post("/api/notes", function (req, res) {
-    var newNote = req.body;
+// making an id from length of notes array.
+let noteId = notes.length;
 
-    newNote["id"] = currentID +1;
-    currentID++;
+//add the notes to db json
+app.post("/api/notes", function (req, res) {
+    const newNote = req.body;
+
+    newNote["id"] = noteId +1;
+    noteId++;
     console.log(newNote);
 
     notes.push(newNote);
+    console.log(">>>>", notes)
 
     writeFileSync();
 
@@ -51,44 +55,20 @@ app.post("/api/notes", function (req, res) {
 
 app.delete("/api/notes/:id", function (req, res) {
     res.send('Got a DELETE request at /api/notes/:id')
-// This allows the test note to be the original note.
+    // getting the id from request inorder to delete the note
     const id = req.params.id;
 
-    let lowestId = notes.filter(function (less) {
-        return less.id < id;
-    });
+    const minId = notes.filter (x => x.id < id);
 
-    let highestId = notes.filter(function (greater) {
-        return greater.id > id;
-    });
 
-    notes = lowestId.concat(highestId);
+    const maxId = notes.filter( x => x.id > id);
+
+    notes = minId.concat(maxId);
 
     writeFileSync();
 })
 
-//==============================================================================
-// Gotta link to my assets!
-
-app.use(express.static("public"));
-
-
-//==============================================================================
-// Gotta link to my assets!
-
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-
-
-
-
-function writeFileSync() {
+const writeFileSync = () => {
     fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
         if (err) {
             console.log("error")
@@ -102,5 +82,5 @@ function writeFileSync() {
 //make the app listen to the server
 
 app.listen(PORT, function () {
-  console.log("API server now listening to port" + PORT);
+  console.log("YAY! server is listening now on port ... " + PORT);
 });
